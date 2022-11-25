8662fcc2219843e7d95220bef1e174153341d0ea
can101 <can896373@gmail.com>
2022-11-25 18:17:09 +0300

    operators added

---
+++ operators.rb
@@ -0,0 +1,44 @@
+# arithmetic operators
+x=5.0
+y=3.0
+puts x+y
+puts x-y
+puts x*y
+puts x/y
+puts x%y
+puts x**y
+
+# assignment operators
+
+i=12
+i+=5
+puts i
+i-=5
+puts i
+i*=5
+puts i
+i/=5
+puts i
+i%=5
+puts i
+i**=5
+puts i
+
+# logical operators
+
+puts x==y
+puts x!=y
+puts x>y
+puts x<y
+puts x>=y
+puts x<=y
+puts x<=>y
+puts String ==="ruby"
+
+x,y,z=5,6,7
+puts y>x and x<z
+puts y>x && x<z
+puts y > x or x > z
+puts y > x || x > z
+puts not(x < z)
+puts !(x < z)
