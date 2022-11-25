cf21cbdc4fc20ff6382346faf84ae97e5b4ca5cd
can101 <can896373@gmail.com>
2022-11-25 19:34:40 +0300

    hashes file created and hash used

---
+++ hashes.rb
@@ -0,0 +1,15 @@
+# 1.way with instance
+# hashes=Hash.new
+# hashes[:key]="value"
+# puts hashes
+# puts hashes[:key]
+# 2.way with brakets []
+# hashes2=Hash[]
+# hashes2[:key]="value"
+# p hashes2
+# p hashes2[:key]
+# 3.way with curly brackets {}
+hashes3={}
+hashes3[:"1234"]="string"
+puts hashes3
+puts hashes3[:"1234"]
