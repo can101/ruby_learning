61da1744ab18e03a3e8d25a22306372ec814afe0
can101 <can896373@gmail.com>
2022-11-25 18:44:24 +0300

    integer and methods used

---
+++ integer.rb
@@ -0,0 +1,14 @@
+# x,y,z=1,2,"1"
+
+# puts x+y
+# # puts z+x because throw error type
+# puts z.to_i+x
+# puts 4.is_a? Integer
+# puts 4.integer?
+# puts 2.even?
+# puts 3.odd?
+# puts "#{-2.abs} #{2.abs}"
+# puts 25.gcd(2) # ebob
+# puts 25.lcm(2) #ekok
+puts Math.sqrt(9)
+puts Math.sqrt(2)
