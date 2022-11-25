f4a4680f7706baede7246d7847c25ee760d3f4ec
can101 <can896373@gmail.com>
2022-11-25 19:10:46 +0300

    boolean file created and boolean methods used

---
+++ boolean.rb
@@ -0,0 +1,23 @@
+# puts 1==1
+# puts 1==2
+# puts "string".is_a? String
+# puts "string".is_a? Numeric
+
+# a,b,c,d=1,false,nil,"string"
+
+# if a
+# puts "is 'a' value true?"
+# end
+# if b
+# puts "is 'b' value true?"
+# end
+# if c
+# puts "is 'c' value true?"
+# end
+# if d
+# puts "is 'd' value true?"
+# end
+
+puts (1==2)&(1==1) # AND operator
+puts (1==2)|(1==1) # OR operator
+puts (1==2)^(1==1) # XOR operator
