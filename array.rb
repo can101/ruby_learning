55ad23190fab7cad27c37eaf2b8cf570f7fe4a10
can101 <can896373@gmail.com>
2022-11-25 19:24:14 +0300

    array file created and use some methods

---
+++ array.rb
@@ -0,0 +1,26 @@
+# arr= Array.new
+# arr=%w[]
+arr=[-4,145,-3,6,-2,-1,0,1,2,3,4]
+# arr.each do |ruby|
+#     p ruby
+# end
+
+# arr.each {|ruby| puts ruby }
+# p arr
+# arr.clear
+# p arr
+# puts "#{arr.size} #{arr.length}"
+# arr.delete(3)
+# p arr
+# p arr
+# arr.delete_if do |del|
+#     del>0
+# end
+# arr.delete_if {|del| del>0}
+# p arr
+# p arr.sort
+t=[]
+p arr.empty?
+p t.empty?
+p arr.first
+p arr.last
